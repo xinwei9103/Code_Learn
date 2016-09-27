4f9ccf53c98bba26f03fa0327db6493924f2d745
xinwei <wxw910322@gmail.com>
2016-09-26 22:54:59 -0500

    9/26/2016

--- src/com/xinwei/scala/foundation/Trait.scala
+++ src/com/xinwei/scala/foundation/Trait.scala
@@ -6,11 +6,25 @@ package com.xinwei.scala.foundation
 object Trait {
   def main(args: Array[String]) {
     //混入trait
+    /*
     val logger = new ConcreteLogger with TraitLogger
     logger.print()
+    */
+    /*
+    val pianoTeacher = new PianoTeacher("xinwei")
+    pianoTeacher.teach
+    pianoTeacher.playPiano
 
+    //匿名类
+    val human = new Human("xinwei") with TTeacher with PianoPlayer{
+      override def teach: Unit = println("teaching")
+    }
+    */
+
+    //AOP 混入
+    val work = new Work with AAction
+    work.doAction
   }
-
 
 }
 
@@ -41,3 +55,52 @@ trait TraitLogger extends Logger{
     println("trait logger")
   }
 }
+
+class Human private{
+  println("Human")
+  private var name:String = _
+  def this(name:String) {
+    this
+    this.name =name
+  }
+  def printName(): Unit ={
+    println(name)
+  }
+}
+
+trait TTeacher extends Human{
+  println("TTeacher")
+  def teach
+}
+
+trait PianoPlayer extends Human{
+  println("PianoPlayer")
+  def playPiano: Unit ={
+    println("play piano")
+  }
+}
+//由左向右按顺序构建
+class PianoTeacher(name:String) extends Human(name) with TTeacher with PianoPlayer{
+  println("PianoTeacher")
+  override def teach: Unit ={
+    println("teaching")
+  }
+}
+
+//AOP
+trait Action{
+  def doAction
+}
+
+trait AAction extends Action{
+  abstract override def doAction: Unit ={
+    println("before")
+    super.doAction
+    println("after")
+  }
+}
+
+class Work extends Action{
+  override def doAction: Unit = println("action")
+}
+
